50ecd43bb23f0401ec1ac4b4a837f758c1fe91cf
Mikhael <mick.dunaev@yandex.ru>
2018-12-05 12:55:45 +0300

    view page indicator

--- app/src/main/java/ru/yandex/dunaev/mick/mystartpage/MainActivity.kt
+++ app/src/main/java/ru/yandex/dunaev/mick/mystartpage/MainActivity.kt
@@ -2,9 +2,12 @@ package ru.yandex.dunaev.mick.mystartpage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.*
 import androidx.lifecycle.ViewModelProviders
+import androidx.viewpager.widget.ViewPager
+import com.rd.animation.type.AnimationType
 import ru.yandex.dunaev.mick.mystartpage.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +20,19 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.viewModel = viewModel
         binding.viewPager.adapter = StartPagerAdapter(supportFragmentManager, ::onNext, ::onSkip)
+
+        binding.pageIndicatorView.setAnimationType(AnimationType.DROP)
+
+        binding.viewPager.addOnPageChangeListener(object: ViewPager.OnPageChangeListener{
+            override fun onPageScrollStateChanged(state: Int) {}
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
+
+            override fun onPageSelected(position: Int) {
+                binding.pageIndicatorView.selection = position
+                binding.pageIndicatorView.visibility = if(position == 2 ) View.GONE else View.VISIBLE
+            }
+        })
 
     }
 
